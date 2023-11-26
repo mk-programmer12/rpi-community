2bebc4881ed08af3ad3e4dda0ffcd5d3c26b90bb
Monir <monirhossain0621@gmail.com>
2023-11-26 23:51:16 +0600

    worked with delete profile picture

--- include/delete_picture.php
+++ include/delete_picture.php
@@ -1,5 +1,16 @@
 <?php
 session_start();
 require_once('connect.php');
+require_once('logger_info.php');
 auth();
+
+$deleteProfilePicture = mysqli_query($connect, "UPDATE `student_info` SET `avatar`='default.png' WHERE `id`='$loggerId'");
+
+if($deleteProfilePicture == true) {
+    header("location: ../profile.php");
+}else {
+    header("location: ../profile.php");
+}
+
+
 ?>
